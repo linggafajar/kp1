e358e4892f21e9729f1a137baf50794c9832c009
linggafajar <148311683+linggafajar@users.noreply.github.com>
2025-06-06 00:19:09 +0700

    Update route.ts

--- src/app/api/permintaan/route.ts
+++ src/app/api/permintaan/route.ts
@@ -1,3 +1,4 @@
+import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     // Contoh ambil userId dari token/session, sesuaikan implementasimu
